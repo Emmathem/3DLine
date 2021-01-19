f83a041636bf59a35f351e246ba532980b16b8c1
Falua Temitope Oyewole <emmat0616@gmail.com>
2021-01-19 21:14:53 +0100

    Assets modified

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
 import AppHeader from "../shared/AppHeader";
 import {AppFooter} from "../shared/AppFooter";
+// import {Row, Col} from 'react-bootstrap'
+import {Sidebar} from "./Sidebar";
 
 class Dashboard extends Component {
   render() {
     return (
         <div>
-          <AppHeader />
-          <h3>Welcome to Covid-19 Dashboard</h3>
-          <AppFooter />
+            <AppHeader />
+            <div className="pageContainer">
+              <div className="row">
+                <div className="col-md-2">
+                  <Sidebar />
+                </div>
+                <div className="col-md-9">
+                  <h3>Welcome to Covid-19 Dashboard</h3>
+                </div>
+              </div>
+              {/*<Row>
+                <Col md={3}>
+                  <Sidebar />
+                </Col>
+                <Col md={9}>
+                  <h3>Welcome to Covid-19 Dashboard</h3>
+                </Col>
+              </Row>*/}
+            </div>
+            <AppFooter />
         </div>
     )
   }
---
+++ src/components/dashboard/Sidebar.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export class Sidebar extends Component {
+  render() {
+    return (
+        <div>
+          <div className="sidebarContainer">Sidebar</div>
+        </div>
+    )
+  }
+}
